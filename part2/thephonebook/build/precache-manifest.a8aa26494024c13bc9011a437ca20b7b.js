f34e630c7bcccafdbc03f3f05cc8b373d3f0144f
2940cristian <2940cristian@gmmail.com>
2020-07-08 02:19:18 -0700

    Heroku

---
+++ part2/thephonebook/build/precache-manifest.a8aa26494024c13bc9011a437ca20b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb851e1a15e3348f2961e35db2c5c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cba7f2f13921106b0ac",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67f172b2a5a704a1b632",
+    "url": "/static/js/2.7c1614a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c1614a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cba7f2f13921106b0ac",
+    "url": "/static/js/main.3fc1f027.chunk.js"
+  },
+  {
+    "revision": "c771fc46209c33cf411a",
+    "url": "/static/js/runtime-main.3a2acdf3.js"
+  }
+]);
